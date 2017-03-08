24334a3b51390016a71f3dbcb6d00f2b10ac465c
Serena Jeblee <sjeblee@gmail.com>
2017-03-08 12:35:36 -0500

    Add randomization to the list in ScrollingActivity
    * Also add some more things to the list
    * Make the font a little smaller

--- code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/ScrollingActivity.java
+++ code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/ScrollingActivity.java
@@ -5,10 +5,11 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 /**
  * Displays the list for the scrolling task
@@ -22,11 +23,10 @@ public class ScrollingActivity extends WearableActivity {
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_launcher_view);
 
-        List<String> itemList = new ArrayList<String>();
-        itemList.add("One");
-        itemList.add("Two");
-        itemList.add("Three");
-        itemList.add("Four");
+        // Load list items from xml and shuffle it
+        ArrayList<String> itemList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.items)));
+        long seed = System.nanoTime();
+        Collections.shuffle(itemList, new Random(seed));
 
         RecyclerView.Adapter mAdapter = new RecyclerListAdapter(itemList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -35,5 +35,6 @@ public class ScrollingActivity extends WearableActivity {
         recyclerView.setAdapter(mAdapter);
 
         // TODO: add timing
+        long starttime = System.currentTimeMillis();
     }
 }
