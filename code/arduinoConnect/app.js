b32877640fa5a4c1db5a5f54365bbe2c190e52c7
Shamama Khattak <shamama@Shamamas-MacBook-Pro.local>
2017-03-23 15:42:56 -0400

    Added code to load onto arduino for touch detection + webserver to pass touch information

---
+++ code/arduinoConnect/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// Set views path
+app.use(express.static(__dirname + '/'));
+
+// Middleware
+app.use(bodyParser.json());       
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+
+// Initialize serial port to read from 
+var serialport = require('serialport');
+var portName = '/dev/cu.usbmodem14141';
+
+var sp = new serialport(portName, {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialport.parsers.readline("\r\n")
+});
+
+// Globals 
+var buf = []
+
+// Read data from serial port and add to buff
+sp.on('data', function(input) {
+	buf.push(input)
+});
+
+// Get navigation bar 
+app.get('/touchInfo', function(req, res) {
+	console.log(buf);
+    res.send(buf);
+    buf = [];
+});  
+
+// Start the server and socket 
+var server = app.listen(3000);
+console.log("listening on port 3000")
---
+++ code/touchDetect/touchDetect.ino
@@ -0,0 +1,46 @@
+#include <ADCTouch.h>
+ 
+int ref0, ref1, ref2, ref3, ref4, ref5;       //reference values to remove offset
+ 
+void setup()
+{
+    // No pins to setup, pins can still be used regularly, although it will affect readings
+ 
+    Serial.begin(9600);
+ 
+    ref0 = ADCTouch.read(A0, 500);    //create reference values to
+    ref1 = ADCTouch.read(A1, 500);      //account for the capacitance of the pad
+    ref2 = ADCTouch.read(A2, 500);    
+    ref3 = ADCTouch.read(A3, 500);
+    ref4 = ADCTouch.read(A4, 500);    
+    ref5 = ADCTouch.read(A5, 500);
+}
+ 
+void loop()
+{
+    String iter; 
+    
+    int value0 = ADCTouch.read(A0);   
+    int value1 = ADCTouch.read(A1);     
+    int value2 = ADCTouch.read(A2);   
+    int value3 = ADCTouch.read(A3);
+    int value4 = ADCTouch.read(A4);   
+    int value5 = ADCTouch.read(A5);
+ 
+    value0 -= ref0;       //remove offset
+    value1 -= ref1;       //remove offset
+    value2 -= ref2;       //remove offset
+    value3 -= ref3;       //remove offset
+    value4 -= ref4;       //remove offset
+    value5 -= ref5;
+
+    iter = "" + (String)(value0 > 10) + "\t";
+    iter = iter + (String)(value1 > 10) + "\t";
+    iter = iter + (String)(value2 > 10) + "\t";
+    iter = iter + (String)(value3 > 10) + "\t";
+    iter = iter + (String)(value4 > 10) + "\t";
+    iter = iter + (String)(value5 > 10) + "\t";
+    
+    Serial.println(iter);
+    delay(100);
+}
