601aeba405d5d1d5cfa4692ebeeedcfbb787e8fb
Serena Jeblee <sjeblee@gmail.com>
2017-03-23 21:17:47 -0400

    Add code to interpret touch events to Android app

--- code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/MainActivity.java
+++ code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/MainActivity.java
@@ -1,38 +1,21 @@
 package edu.toronto.touchband.touchbandapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.wearable.activity.WearableActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.android.volley.Response;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 public class MainActivity extends WearableActivity {
 
-    private final String mUrl = "http://www.yahoo.com";
     private static final String sUserIdKey = "user_id";
-    private RequestQueue mRequestQueue;
     private MetricsManager mMetricsManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Context mContext = this;
-
-        // Start the HTTP request queue
-        mRequestQueue = Volley.newRequestQueue(mContext);
-        StringRequest stringRequest = createStringRequest();
-        mRequestQueue.add(stringRequest);
 
         MetricsManager.initInstance(this);
         mMetricsManager = MetricsManager.getInstance();
@@ -59,55 +42,4 @@ public class MainActivity extends WearableActivity {
         startActivity(intent);
     }
 
-    public void generateTouch(int startx, int starty, int endx, int endy) {
-        // TODO: generate a touch event on the main thread
-        //System.out.println("Generated touch event (not really)");
-
-        // TODO: get the current view
-        View view = getWindow().getCurrentFocus();
-
-        // Obtain MotionEvent object
-        long downTime = SystemClock.uptimeMillis();
-        long eventTime = SystemClock.uptimeMillis() + 100;
-        float x = 0.0f;
-        float y = 0.0f;
-        // List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
-        int metaState = 0;
-        MotionEvent motionEvent = null;
-        if ((startx == endx) && (starty == endy)) {
-            motionEvent = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_UP, x, y, metaState);
-        } else {
-            motionEvent = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_MOVE, x, y, metaState);
-        }
-
-
-        // Dispatch touch event to view
-        //view.dispatchTouchEvent(motionEvent);
-    }
-
-    /**
-     * Create a new HTTP GET request that generates another request on the successful completion of this one
-     * @return the StringRequest
-     */
-    public StringRequest createStringRequest() {
-        final StringRequest stringRequest = new StringRequest(Request.Method.GET, mUrl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //System.err.println("Response is: " + response);
-                        // TODO: get touch info from the response
-                        // TODO: figure out if an up, down, or move event has happened
-                        generateTouch(0, 0, 0, 0);
-
-                        // Send the next request
-                        mRequestQueue.add(createStringRequest());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.err.println("Error from Volley GET request!");
-            }
-        });
-        return stringRequest;
-    }
 }
--- code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/ScrollingActivity.java
+++ code/TouchBandApp/app/src/main/java/edu/toronto/touchband/touchbandapp/ScrollingActivity.java
@@ -1,19 +1,31 @@
 package edu.toronto.touchband.touchbandapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.wearable.activity.WearableActivity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 /**
  * Displays the list for the scrolling task
@@ -27,10 +39,26 @@ public class ScrollingActivity extends WearableActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView mRecyclerView;
 
+    private RequestQueue mRequestQueue;
+    private final String mUrl = "http://172.31.35.62:3000/touchInfo";
+
+    private AtomicBoolean mTouch = new AtomicBoolean(false);
+    private AtomicIntegerArray mTouchIndices = new AtomicIntegerArray(6);
+    private AtomicInteger mPrevTouchLoc = new AtomicInteger(-1);
+    private int yScale = 60;
+    private int mThresh = 80;
+    //private int mXvalue = 160;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+
+        // Start the HTTP request queue
+        final Context mContext = this;
+        mRequestQueue = Volley.newRequestQueue(mContext);
+        StringRequest stringRequest = createStringRequest();
+        mRequestQueue.add(stringRequest);
 
         // Get the target item
         Intent intent = this.getIntent();
@@ -46,11 +74,11 @@ public class ScrollingActivity extends WearableActivity {
         int numItems = itemList.size();
         long seed = System.nanoTime();
         Collections.shuffle(itemList, new Random(seed));
-        int index = ThreadLocalRandom.current().nextInt(5, numItems+1);
+        int index = ThreadLocalRandom.current().nextInt(5, numItems + 1);
         itemList.add(index, mTargetItem);
 
         // Pad the list with extra values so the last one can be reached
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             itemList.add("");
         }
 
@@ -61,14 +89,15 @@ public class ScrollingActivity extends WearableActivity {
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(ScrollingActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override public void onItemClick(View view, int position) {
+                    @Override
+                    public void onItemClick(View view, int position) {
                         long endtime = System.currentTimeMillis();
                         String item = ((RecyclerListAdapter) mAdapter).getCurrentItem();
 
                         // Check to see if it's the right item, if so, return to start screen
                         if (item.equals(mTargetItem)) {
                             long time = endtime - mStartTime;
-                            double acc = 1.0/(mWrongSelections+1);
+                            double acc = 1.0 / (mWrongSelections + 1);
                             recordMetric(time, acc);
                             ScrollingActivity.this.finish();
                         } else {
@@ -78,20 +107,24 @@ public class ScrollingActivity extends WearableActivity {
                 })
         );
 
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 //System.out.println("scroll state changed");
             }
+
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                System.out.println("Scrolled by " + dy);
                 int selectedIndex = ((RecyclerListAdapter) mAdapter).getSelectedIndex();
                 int itemIndex = ((RecyclerListAdapter) mAdapter).onScrolled(dy);
                 // Update item highlight
                 if (selectedIndex != itemIndex) {
                     RecyclerView.LayoutManager layoutManager = mRecyclerView.getLayoutManager();
                     View oldView = layoutManager.findViewByPosition(selectedIndex);
-                    oldView.setSelected(false);
+                    if (oldView != null) {
+                        oldView.setSelected(false);
+                    }
                     View itemView = layoutManager.findViewByPosition(itemIndex);
                     itemView.setSelected(true);
                 }
@@ -101,7 +134,156 @@ public class ScrollingActivity extends WearableActivity {
         mStartTime = System.currentTimeMillis();
     }
 
+    @Override
+    public void onDestroy() {
+        mRequestQueue.stop();
+        super.onDestroy();
+    }
+
     private void recordMetric(long time, double acc) {
         mMetricsManager.recordMetric("scrolling", time, acc);
+    }
+
+    public void generateTouch(int eventType, int start, int end) {
+        System.out.println("Generating touch event " + eventType + ", " + start + ", " + end);
+
+        // Obtain MotionEvent object
+        long downTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis() + 100;
+        float x = 0.0f;
+        float y = 0.0f;
+        // List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        int metaState = 0;
+        MotionEvent motionEvent = null;
+        if ((eventType == MotionEvent.ACTION_DOWN) || (eventType == MotionEvent.ACTION_UP)) {
+            motionEvent = MotionEvent.obtain(downTime, eventTime, eventType, 0, end, metaState);
+        } else if (eventType == MotionEvent.ACTION_MOVE) {
+            System.out.println("Creating move event");
+            MotionEvent.PointerCoords[] pointerCoords = new MotionEvent.PointerCoords[1];
+            MotionEvent.PointerCoords pc = new MotionEvent.PointerCoords();
+            pc.setAxisValue(MotionEvent.AXIS_Y, end);
+            pc.setAxisValue(MotionEvent.AXIS_X, 0);
+            pointerCoords[0] = pc;
+            MotionEvent.PointerProperties pointerProps = new MotionEvent.PointerProperties();
+            pointerProps.id = 0;
+            MotionEvent.PointerProperties[] pointerProperties = {pointerProps};
+            motionEvent = MotionEvent.obtain(downTime, eventTime, eventType, 1, pointerProperties, pointerCoords, metaState, 0,
+                    0.01f, 0.01f, 0, 0, 0, 0);
+        }
+
+        // Dispatch touch event to view
+        if (motionEvent != null) {
+            // Get the current view
+            //View view = getWindow().getCurrentFocus();
+            System.out.println("dispatch touch event");
+            this.dispatchTouchEvent(motionEvent);
+        }
+    }
+
+    /**
+     * Create a new HTTP GET request that generates another request on the successful completion of this one
+     *
+     * @return the StringRequest
+     */
+    public StringRequest createStringRequest() {
+        final StringRequest stringRequest = new StringRequest(Request.Method.GET, mUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Get touch info from the response
+                        if (response.length() > 2) {
+                            // Split off the []
+                            String string = response.substring(1, response.length() - 1);
+                            //System.out.println("string: " + string);
+                            String[] arrays = string.split(",");
+                            // Figure out if an up, down, or move event has happened
+                            //for (String arrayString : arrays) {
+                            String arrayString = arrays[arrays.length - 1];
+                            arrayString = arrayString.substring(1, arrayString.length() - 1);
+                            System.out.println("arraystring: " + arrayString);
+                            int[] array = new int[6];
+                            String[] sarray = arrayString.split(" ");
+                            for (int k = 0; k < 6; k++) {
+                                array[k] = Integer.parseInt(sarray[k]);
+                            }
+
+                            boolean moved = false;
+                            boolean started = false;
+                            int newTouchLoc = -1;
+                            int maxIndex = -1;
+                            int val = mThresh;
+
+                            for (int y = 0; y < array.length; y++) {
+                                if (array[y] > val) {
+                                    maxIndex = y;
+                                    val = array[y];
+                                }
+                            } // end for
+
+                            int y = maxIndex;
+                            System.out.println("maxIndex: " + maxIndex);
+                            double touchIndex = -1.0;
+
+                            if (maxIndex != -1) {
+                                int totalval = val;
+                                int sum = val * maxIndex;
+                                for (int z = maxIndex - 1; z >= 0; z--) {
+                                    if (array[z] < mThresh) {
+                                        break;
+                                    }
+                                    totalval += array[z];
+                                    sum += (z * array[z]);
+                                }
+                                for (int r = maxIndex + 1; r < 6; r++) {
+                                    if (array[r] < mThresh) {
+                                        break;
+                                    }
+                                    totalval += array[r];
+                                    sum += (r * array[r]);
+                                }
+
+                                touchIndex = (double) sum / (double) totalval;
+                                newTouchLoc = (int) (touchIndex * (double) yScale);
+                                System.out.println("touchIndex: " + touchIndex + "newTouchLoc: " + newTouchLoc);
+                            }
+
+
+                            System.out.println("PrevTouchLoc: " + mPrevTouchLoc.get());
+                            if (mTouch.get() && (newTouchLoc != (mPrevTouchLoc.get())) && (maxIndex != -1)) {
+                                moved = true;
+                            } else if ((maxIndex != -1) && !mTouch.get()) {
+                                started = true;
+                            }
+
+                            // Check for ACTION_UP
+                            if (mTouch.get() && maxIndex < 0) {
+                                generateTouch(MotionEvent.ACTION_UP, mPrevTouchLoc.get(), mPrevTouchLoc.get());
+                                mTouch.compareAndSet(true, false);
+                            } else { // ACTION_DOWN or ACTION_MOVE
+                                if (started) {
+                                    mTouch.compareAndSet(false, true);
+                                    generateTouch(MotionEvent.ACTION_DOWN, newTouchLoc, newTouchLoc);
+                                } else if (moved) {
+                                    generateTouch(MotionEvent.ACTION_MOVE, mPrevTouchLoc.get(), newTouchLoc);
+                                }
+                            }
+
+                            // Save new touch info
+                            for (int j = 0; j < array.length; j++) {
+                                mTouchIndices.set(j, array[j]);
+                            }
+                            mPrevTouchLoc.set(newTouchLoc);
+                        }
+
+                        // Send the next request
+                        mRequestQueue.add(createStringRequest());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.err.println("Error from Volley GET request!");
+            }
+        });
+        return stringRequest;
     }
 }
